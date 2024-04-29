6fbeaacefe6985ed78d63dc4a82698a2f9a79998
Sherry90 <hhlsdkm@gmail.com>
2024-04-29 16:58:23 +0900

    feat: random return

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,7 +1,11 @@
+const words = ["Sherry", "Bourbon"];
+
 const Post = () => {
+  const chooseWord = Math.random() > 0.5 ? words[0] : words[1];
+  
   return (
     <div>
-      <p>Sherry</p>
+      <p>{chooseWord}</p>
       <p>Whisky Daisuki</p>
     </div>
   );
