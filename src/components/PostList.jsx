66d3c21cdc92aeca274cce657d7660695153de4a
Sherry90 <hhlsdkm@gmail.com>
2024-05-01 17:34:39 +0900

    feat: state lifting, transit state, setState

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,28 @@
 import Post from "./Post.jsx";
 import NewPost from "./NewPost.jsx";
 import classes from "./PostList.module.css";
+import { useState } from "react";
 
 const PostList = () => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onBodyChange={bodyChangeHandler}
+        onAuthorChange={authorChangeHandler}
+      />
       <ul className={classes.posts}>
-        <Post author={"Sherry"} body={"Bug Report"} />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author={"Cask"} body={"Base Woods"} />
       </ul>
     </>
