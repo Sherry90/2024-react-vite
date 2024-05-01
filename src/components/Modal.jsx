eb1956cf1639c5f9305042c99b663f917b13429f
Sherry90 <hhlsdkm@gmail.com>
2024-05-01 18:06:45 +0900

    feat: set css

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,10 @@
+import classes from "./Modal.module.css"
+
 const Modal = () => {
-  
+  return <>
+    <div className={classes.backdrop} />
+    <dialog className={classes.modal}></dialog>
+  </>
 };
 
 export default Modal;
