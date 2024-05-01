a31cf8f573f25252d39454e809efa1c8abbc34b1
Sherry90 <hhlsdkm@gmail.com>
2024-05-01 17:33:31 +0900

    remove: state, eventHandler code

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,21 +1,12 @@
-import { useState } from "react";
-
 import classes from "./NewPost.module.css";
 
 const NewPost = () => {
-  const [enteredBody, setEnteredBody] = useState("");
-
-  const changeBodyHandler = (event) => {
-    setEnteredBody(event.target.value);
-  };
-
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
-      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
