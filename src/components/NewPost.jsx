0d0fe2890ac2123e1a49e4f77afb9dc3ed81560f
Sherry90 <hhlsdkm@gmail.com>
2024-05-01 17:12:18 +0900

    feat: use State

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,22 +1,27 @@
-import classes from './NewPost.module.css';
+import { useState } from "react";
+
+import classes from "./NewPost.module.css";
 
 const NewPost = () => {
+  const [enteredBody, setEnteredBody] = useState("");
+
   const changeBodyHandler = (event) => {
-    console.log(event.target.value);
+    setEnteredBody(event.target.value);
   };
-  
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
       </p>
     </form>
   );
-}
+};
 
 export default NewPost;
