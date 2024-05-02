24e21c1631311cf061883805c96b60c114cc11b2
Sherry90 <hhlsdkm@gmail.com>
2024-05-02 16:42:17 +0900

    feat: modal Cancel event handler

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -24,6 +24,7 @@ const PostList = ({ isPosting, onStopPosting }) => {
           <NewPost
             onBodyChange={bodyChangeHandler}
             onAuthorChange={authorChangeHandler}
+            onCancel={onStopPosting}
           />
         </Modal>
       )}
