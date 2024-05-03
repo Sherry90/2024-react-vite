76ab3363ac7f180afe87b74c95f65ff4a5fdc382
Sherry90 <hhlsdkm@gmail.com>
2024-05-03 21:05:17 +0900

    feat: moving form input handler

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,15 +1,29 @@
+import { useState } from "react";
+
 import classes from "./NewPost.module.css";
 
 const NewPost = ({ onBodyChange, onAuthorChange, onCancel }) => {
+const NewPost = ({ onCancel }) => {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
+        <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onAuthorChange} />
+        <input type="text" id="name" required onChange={authorChangeHandler} />
       </p>
       <p className={classes.actions}>
         <button type={"button"} onClick={onCancel}>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,35 +1,17 @@
-import { useState } from "react";
-
 import Post from "./Post.jsx";
 import NewPost from "./NewPost.jsx";
 import Modal from "./Modal.jsx";
 import classes from "./PostList.module.css";
 
 const PostList = ({ isPosting, onStopPosting }) => {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  const bodyChangeHandler = (event) => {
-    setEnteredBody(event.target.value);
-  };
-
-  const authorChangeHandler = (event) => {
-    setEnteredAuthor(event.target.value);
-  };
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author={"Cask"} body={"Base Woods"} />
       </ul>
     </>
