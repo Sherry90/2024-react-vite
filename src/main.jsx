31ebfa82cf01e793469a2b470204a1daf29582f5
Sherry90 <hhlsdkm@gmail.com>
2024-05-05 18:15:11 +0900

    feat: use Router to NewPost

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from './App'
 import './index.css'
+import NewPost from "./components/NewPost.jsx";
 
 const router = createBrowserRouter ([
-  { path: "/", element: <App/> }
+  { path: "/", element: <App/> },
+  { path: "/create-post", element: <NewPost /> }
 ]);
 
 ReactDOM.createRoot (document.getElementById ('root')).render (
