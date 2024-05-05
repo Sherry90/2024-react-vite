88c25f15a64e500ca0d7666cf7100a47e4c14e0a
Sherry90 <hhlsdkm@gmail.com>
2024-05-05 18:38:51 +0900

    feat: config Layout children

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import App from "./App";
-import "./index.css";
 import NewPost from "./components/NewPost.jsx";
+import RootLayout from "./routes/RootLayout.jsx";
+
+import "./index.css";
 
 const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/create-post", element: <NewPost /> },
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <App /> },
+      { path: "/create-post", element: <NewPost /> },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
