fa417f3e15a001630e4eb0f75371a966c2b70147
Sherry90 <hhlsdkm@gmail.com>
2024-05-05 19:41:38 +0900

    remove: handler & state

--- src/routes/Posts.jsx
+++ src/routes/Posts.jsx
@@ -1,22 +1,10 @@
-import { useState } from "react";
-
 import PostList from "./components/PostList.jsx";
 
 const Posts = () => {
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-
-  const showModalHandler = () => {
-    setModalIsVisible(true);
-  };
-
-  const hideModalHandler = () => {
-    setModalIsVisible(false);
-  };
-
   return (
     <>
       <main>
-        <PostList isPosting={modalIsVisible} onStopPosting={hideModalHandler} />
+        <PostList />
       </main>
     </>
   );
