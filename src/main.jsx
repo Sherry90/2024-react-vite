8e487d2bde26c672fdc1ad5a6b4cecebd0c1eccd
Sherry90 <hhlsdkm@gmail.com>
2024-05-05 18:15:41 +0900

    refactor: format with prettier

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import App from './App'
-import './index.css'
+import App from "./App";
+import "./index.css";
 import NewPost from "./components/NewPost.jsx";
 
-const router = createBrowserRouter ([
-  { path: "/", element: <App/> },
-  { path: "/create-post", element: <NewPost /> }
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/create-post", element: <NewPost /> },
 ]);
 
-ReactDOM.createRoot (document.getElementById ('root')).render (
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
-  </React.StrictMode>
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
