520f1d9da986ddc6dfa5ece0fce26cbc14060e21
Sherry90 <hhlsdkm@gmail.com>
2024-05-06 21:07:39 +0900

    feat: use loader

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import Posts from "./routes/Posts.jsx";
+import Posts, { loader as postLoader} from "./routes/Posts.jsx";
 import NewPost from "./routes/NewPost.jsx";
 import RootLayout from "./routes/RootLayout.jsx";
 
@@ -16,6 +16,7 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Posts />,
+        loader: postLoader,
         children: [{ path: "/create-post", element: <NewPost /> }],
       },
     ],
