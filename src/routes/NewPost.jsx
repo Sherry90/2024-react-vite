9a8f0e00eb76b47ef7eabde79023fd4215bc9760
Sherry90 <hhlsdkm@gmail.com>
2024-05-06 21:26:50 +0900

    feat: action function

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -27,3 +27,15 @@ const NewPost = () => {
 };
 
 export default NewPost;
+
+export const action = async ({request}) => {
+  const formData = await request.formData();
+  const postData = Object.fromEntries(formData);
+  fetch("http://localhost:8080/posts", {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
